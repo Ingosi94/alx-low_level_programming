756fec9944ebde0a574deeae80ae758cb9a8799b
Emmanuelingosi <emmanuelingosi442@gmail.com>
2022-09-09 20:36:31 -0700

    Task 4.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+
+char low, e, q;
+
+e = 'e';
+q = 'q';
+
+for (Low = 'a'; low <= 'z'; low++)
+{
+	if (low != e && low != q)
+		putchar(low);
+}	
