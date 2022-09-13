2f5569d29410ca5c9c622d0e390ed2b7c6562b52
Emmanuelingosi <emmanuelingosi442@gmail.com>
2022-09-12 17:19:48 -0700

    Task 4.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
-#include <stdio.h>                                                                                                                      
-#include <stdlib.h>                                                                                                                     
-#include <time.h>                                                                                                                       
+#include <stdio.h>
 /**
- * main - Entry point                                                                                                             
+ * main - print if the number is postive, zero, or negative
  *
- * Return: Always success                                                                                                              
- */                                                                                                                                     
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
-{                                                                                                                                 
-	
-char low, e, q;                                                                                                                         
-
-e = 'e';                                                                                                                                
-q = 'q';                                                                                                                                
-
-for (low = 'a'; low <= 'z'; low++)                                                                                                      
-{                                                                                                                            		        if (low != e && low != q)    
-       		putchar(low);                                                                                                           
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
 }
